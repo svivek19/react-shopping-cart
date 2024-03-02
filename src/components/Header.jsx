08492f02b3a7f863f562c02d0c2b860b696881c1
svivek19 <svivek030503@gmail.com>
2024-03-02 12:19:15 +0530

    header finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,19 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <div>
-          <h3>Shopping Cart</h3>
-        </div>
-        <ul>
-          <li>
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"/cart"}>Cart</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="bg-slate-800 flex p-4 font-bold text-lg justify-between text-white items-center">
+      <div>
+        <h3>Shopping Cart</h3>
+      </div>
+      <ul className="flex gap-6 mx-4">
+        <li className="hover:text-amber-400">
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li className="hover:text-amber-400">
+          <Link to={"/cart"}>Cart</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
