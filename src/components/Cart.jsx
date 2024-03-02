9a30f9c2b3e778458cb9b2e4eba5e3e98c41f484
svivek19 <svivek030503@gmail.com>
2024-03-02 16:04:12 +0530

    feat: cart item added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Cart = () => {
+const Cart = ({ cart }) => {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+
   return (
     <>
       <h1 className="text-center text-3xl font-bold my-5">Cart Products</h1>
-      <div className="w-11/12 mx-auto mb-4 bg-gray-100 p-5 rounded flex justify-center">
-        <div>
-          <img
-            src="https://www.onestalove.in/wp-content/uploads/2021/08/Home-Banner-Pizza.png"
-            alt="Image"
-            width={100}
-          />
-        </div>
-        <div className="items-center mx-8 my-5">
-          <h3 className="text-2xl">Product Name</h3>
-          <p className="text-xl">$36 </p>
-        </div>
+      <div className="w-11/12 mx-auto space-y-5 bg-gray-100 p-5 rounded">
+        {cart.map((product, index) => (
+          <div key={index} className="flex flex-row">
+            <div>
+              <img
+                src={product.image}
+                alt={`${product.name} - Image`}
+                width={100}
+              />
+            </div>
+            <div className="items-center mx-8 my-5">
+              <h3 className="text-2xl">{product.name}</h3>
+              <p className="text-xl">${product.price} </p>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1 className="mx-5 text-2xl my-10">
-        Total Amount:{" "}
-        <span className="font-bold mx-2 text-red-500">Rs: 230</span>
-      </h1>
+      <div className="mx-5 text-2xl my-10">
+        Total Amount:
+        <span className="font-bold mx-2 text-red-500">₹{total}</span>
+      </div>
     </>
   );
 };
